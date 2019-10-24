0ff04f2e627e99463e1b2e78a98e6aeb77a8596f
Rasul Kireev <kireevr1996@gmail.com>
2019-10-24 03:06:21 -0400

    MVP

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,9 @@
+from django.contrib import admin
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, emailView, successView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('email/', emailView, name='email'),
+    path('email-success/', successView, name='email-success')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,32 @@
 from django.views.generic import TemplateView
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .forms import ContactForm
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
 class LoggedInHomePageView(TemplateView):
     template_name = 'home.html'
+
+
+
+def emailView(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, ['admin@example.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('success')
+    return render(request, "email.html", {'form': form})
+
+def successView(request):
+    return HttpResponse('Success! Thank you for your message.')
